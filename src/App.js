44f648e39e16253d7cf2813fee9b21f565040a7e
Guilherme Nunes Amaral dos Santos <guilherme.santos@vikservices.com.br>
2022-09-10 23:15:22 -0300

    C

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Alert from 'react-bootstrap/Alert'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => (
+  <Col>
+ <Alert variant='primary'>
+  Alo
+ </Alert>
+ <Button>TESTE</Button>
+ </Col>
+);
 
 export default App;
